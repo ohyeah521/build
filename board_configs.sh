6a867f13b6bb9439105bbfbea201812103260f54
Jack Ma <jack@vamrs.com>
2018-04-02 16:13:23 +0800

    Add ficus build boards config

--- board_configs.sh
+++ board_configs.sh
@@ -28,6 +28,14 @@ case ${BOARD} in
 		export CROSS_COMPILE=aarch64-linux-gnu-
 		CHIP="rk3399"
 		;;
+	"ficus")
+		DEFCONFIG=rockchip_linux_defconfig
+		UBOOT_DEFCONFIG=evb-rk3399_defconfig
+		DTB=ficus-linux.dtb
+		export ARCH=arm64
+		export CROSS_COMPILE=aarch64-linux-gnu-
+		CHIP="rk3399"
+		;;
 	"rk3399-firefly")
 		DEFCONFIG=rockchip_linux_defconfig
 		UBOOT_DEFCONFIG=firefly-rk3399_defconfig
