eadf44f3073717fcf53e03c3868fca6302a0f587
Stephen Chen <stephen@radxa.com>
2022-10-19 09:40:28 +0800

    mk-boot.sh: update rk356x bl31.elf
    Signed-off-by: Stephen Chen <stephen@radxa.com>

--- mk-uboot.sh
+++ mk-uboot.sh
@@ -342,7 +342,7 @@ EOF
 	cp trust.img ${OUT}/u-boot/
 elif [ "${CHIP}" == "rk3566" ]; then
 	make ${UBOOT_DEFCONFIG}
-	make BL31=../rkbin/bin/rk35/rk3568_bl31_v1.32.elf spl/u-boot-spl.bin u-boot.dtb u-boot.itb
+	make BL31=../rkbin/bin/rk35/rk3568_bl31_set_pmic_sleep_low_20221018_v1.32.elf spl/u-boot-spl.bin u-boot.dtb u-boot.itb
 	./tools/mkimage -n rk3568 -T rksd -d ../rkbin/bin/rk35/rk3566_ddr_1056MHz_v1.10.bin:spl/u-boot-spl.bin idbloader.img
 	cp u-boot.itb ${OUT}/u-boot/
 	cp idbloader.img ${OUT}/u-boot/
@@ -350,7 +350,7 @@ elif [ "${CHIP}" == "rk3566" ]; then
 	generate_spi_image
 elif [ "${CHIP}" == "rk3568" ]; then
 	make ${UBOOT_DEFCONFIG}
-	make BL31=../rkbin/bin/rk35/rk3568_bl31_v1.32.elf spl/u-boot-spl.bin u-boot.dtb u-boot.itb
+	make BL31=../rkbin/bin/rk35/rk3568_bl31_set_pmic_sleep_low_20221018_v1.32.elf spl/u-boot-spl.bin u-boot.dtb u-boot.itb
 	./tools/mkimage -n rk3568 -T rksd -d ../rkbin/bin/rk35/rk3568_ddr_1056MHz_v1.10.bin:spl/u-boot-spl.bin idbloader.img
 	cp u-boot.itb ${OUT}/u-boot/
 	cp idbloader.img ${OUT}/u-boot/
